a73ab8e7aa9e411d4fbd69f48123ac7c416db9b9
Ucastle <skaska007@naver.com>
2025-10-13 12:18:32 +0900

    Feat:회원가입, 로그인 페이지  UI

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,26 @@
+// src/app/mypage/page.tsx
+
+// 추후 mypage/profile/page.tsx로 경로 변경
+// spa 스타일?
+
+// 첫 렌더링시에는 내 정보가 보이는게 좋아보임
+
+// import GNB from "@/components/GNB";
+// import SideMenu from "@/components/SideMenu";
+
+// export default function Mypage() {
+//   return (
+//     <>
+//       <header>
+//         <GNB />
+//       </header>
+//       <aside className="w-[290px]">
+//         <SideMenu />
+//       </aside>
+//     </>
+//   );
+// }
+
+export default function Mypage() {
+  return <div></div>;
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,112 @@
+// src/app/signup/page.tsx
+"use client";
+
+import React, { useState } from "react";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import logoAuth from "@/assets/img/logo_auth.svg";
+
+const wrapper = "min-h-screen flex justify-center items-center";
+
+// 이메일 유효성 검사
+function isValidEmail(email: string): boolean {
+  const effective = /^[^\s@]+@[^\s@]+\.(com|co.kr)$/i;
+  return effective.test(email);
+}
+
+export default function Singup() {
+  const [email, setEmail] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+
+  const isEamilValid = isValidEmail(email); // 검사
+
+  const isValid =
+    isEamilValid &&
+    nickname.trim() !== "" &&
+    password.trim().length >= 8 &&
+    password === checkPassword;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!isValid) return;
+    console.log("폼 제출 성공");
+  };
+
+  return (
+    <div className={wrapper}>
+      <div className="w-[640px]">
+        <p className="mb-12 mt-12">이미지</p>
+        {/* <img src={logoAuth} alt="로고" width={120} height={40} /> */}
+        <form>
+          <Input
+            label="이메일"
+            placeholder="이메일을 입력해주세요."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mb-8"
+            status={!isValidEmail(email) && email ? "error" : "default"}
+            helpText={
+              !isValidEmail(email) && email
+                ? "올바른 이메일을 입력해주세요"
+                : undefined
+            }
+          />
+          <Input
+            label="닉네임"
+            value={nickname}
+            placeholder="닉네임을 입력해 주세요."
+            onChange={(e) => setNickname(e.target.value)}
+            className="mb-8"
+          />
+          <Input
+            label="비밀번호"
+            type="password"
+            placeholder="8자 이상 입력해 주세요."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            showPasswordToggle
+            className="mb-8"
+            status={
+              password.length > 0 && password.trim().length < 8
+                ? "error"
+                : "default"
+            }
+            helpText={
+              password.length > 0 && password.trim().length < 8
+                ? "8자 이상 입력해 주세요."
+                : undefined
+            }
+          />
+          <Input
+            label="비밀번호 확인"
+            type="password"
+            placeholder="비밀번호를 한 번 더 입력해 주세요."
+            value={checkPassword}
+            onChange={(e) => setCheckPassword(e.target.value)}
+            showPasswordToggle
+            className="mb-8"
+            status={
+              checkPassword && checkPassword !== password ? "error" : "default"
+            }
+            helpText={
+              checkPassword && checkPassword !== password
+                ? "비밀번호를 확인해 주세요."
+                : undefined
+            }
+          />
+        </form>
+        <Button
+          type="submit"
+          size="lg"
+          label="회원가입"
+          fullWidth
+          disabled={!isValid}
+          variant={!isValid ? "secondary" : "primary"}
+          onClick={handleSubmit}
+        />
+      </div>
+    </div>
+  );
+}
